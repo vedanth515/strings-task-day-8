fd99342d1338aeaff09578ef34a9f0fd3c058d4a
vedanth515 <vedanthkarnataka@gmail.com>
2024-12-09 18:27:23 +0530

    Add files via upload

---
+++ methods.js
@@ -0,0 +1,158 @@
+
+//////////////object task///////////////
+
+// first question//
+
+// var person ={
+//     firstname:"karnataka",
+//     lastname:"vedanth",
+//     age:22
+// }
+// console.log(person.firstname);
+// person.city="newyork"
+// console.log(person);
+
+// second question//
+
+// var product={
+//     name:"kurkure",
+//     price:"10rs",
+//     instock:"available"
+// }
+// // console.log(product);
+// product.price="20rs"
+// // console.log(product);
+// delete product.instock
+// console.log(product);
+
+// third question //
+
+// var library ={
+//     fantasy:"this is my book",
+//     mystery:"this is not mt book",
+// }
+// library.scienceFiction='"harsha-book" "vedanth-book"'
+// console.log(library);
+// delete library.mystery
+// console.log(library);
+
+ // fourth question//
+// var user ={
+//     username:"akula rahul",
+//     email:"rahulakula25@gmail.com",
+//     address:{
+//       city:"hyderabad",
+//       state:"telagana",
+//       zip:500045,
+//     }
+// }
+// Object.freeze(user)
+// console.log(user);
+// user.email="rahulakula52@gamil.com"
+// console.log(user);
+// Object.isFrozen(user)
+// console.log(user);
+// user.phone="8686376476"
+// console.log(user);
+
+// fifth question//
+
+// var car ={
+//     maker:"kia",
+//     model:"seltos",
+//     price:"1000000",
+
+// }
+
+// // Object.seal()
+// // console.log(car);
+
+// delete car.maker
+// console.log(car);
+// Object.isSealed()
+
+
+
+////////////// strings task ///////////
+//1question//
+
+// var string1 ="    battu"
+// var string2 ="harsha     "
+// var concatstring=string1.concat(string2);
+// // console.log(concatstring);
+// // console.log(concatstring.trim());
+// console.log(concatstring.toUpperCase());
+
+//3question//
+
+// let str1 = "a"
+// let str2 = "pple"
+// let concatstr=str1.concat(str2);
+// // console.log(concatstr);
+// // console.log(concatstr.toUpperCase());
+// console.log(concatstr.slice(3,4));
+
+//4 question//
+
+// var one = "firststring"
+// var two = "secondstring"
+// // console.log(one.slice(0,3));
+// // console.log(two.slice(9,12));
+// var onetwo =one.concat(two);
+
+// a=onetwo[0].toUpperCase() +onetwo.slice(1,22)+onetwo[22].toUpperCase();
+// console.log(a)
+// // console.log(onetwo);
+
+//5 question//
+
+// var z = "      surya     "
+// console.log(z.trim());
+// console.log(z[6].toUpperCase());
+// console.log(z.slice[7,8]);
+
+//6 question//
+
+// var a = "hello there , how are you "
+// console.log(a.length-1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
